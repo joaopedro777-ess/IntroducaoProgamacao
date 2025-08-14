0b3279382a74788854158538acfdcc170941977c
joaopedro777-ess <joaopereirahh69@gmail.com>
2025-08-14 10:38:22 -0300

    Add files via upload

---
+++ 1 2 bimestre.py
@@ -0,0 +1,14 @@
+n1=float(input("Digite a nota do 1° bimestre:"))
+n2=float(input("Digite a nota do 2° bimestre:"))
+n3=((60-(n1*2+n2*2))/2)/3
+print("Você precisa tirar", n3, "nos próximos 2 bimestres" )
+
+
+
+
+
+         
+
+
+
+    
