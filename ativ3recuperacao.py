713fb6462132c3d42a7c1522fb1c4e0f2151140e
joaopedro777-ess <joaopereirahh69@gmail.com>
2025-11-06 10:28:32 -0300

    Add files via upload

---
+++ ativ3recuperacao.py
@@ -0,0 +1,6 @@
+#joão pedro, Márcio, Kauã
+soma=0
+for i in range(200):
+ if i%7==0:
+    soma+=i
+print("A soma dos múltiplos de 7 é igual a:", soma)
---
+++ ativ5prova2recuperacao.py
@@ -0,0 +1,12 @@
+num=int(input("Digite um número:"))
+palin=0
+normal=num
+while normal>0:
+    digito=normal%10
+    palin=palin*10+digito
+    normal=normal//10
+print("Número ao contário", palin)
+if num==palin:
+    print("o número é um palíndromo ")
+else:
+    print("O número não é um palindromo") 
